c39613445859e059f7b56ed3fc0e30531d26e329
Dgirunies <erineu16@gmail.com>
2021-12-21 11:46:52 +0100

    Project Boilerplate

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { DatePickerIOSBase, View } from "react-native";
+
+const App = () => {
+  return <View></View>;
+};
+
+export default () => {
+  return <App />;
+};
---
+++ screens/OnBoarding/OnBoarding.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Animated,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+
+const OnBoarding = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default OnBoarding;
